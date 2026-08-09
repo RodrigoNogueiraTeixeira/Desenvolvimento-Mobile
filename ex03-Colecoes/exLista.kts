c788aa21f4207a88bf241286697c369a99b78d54
Rodrigo Nogueira <digo22nt@gmail.com>
2026-08-09 15:03:17 -0300

    ex 03, ex lista completo

---
+++ ex03-Colecoes/exLista.kts
@@ -0,0 +1,8 @@
+val verdeNumeros = listOf(1, 4, 23)
+val vermelhoNumeros = listOf(17, 2)
+
+var quantidadeIndex = verdeNumeros.count()
+println("Quantidade index Lista Verde: $quantidadeIndex")
+
+quantidadeIndex = vermelhoNumeros.count()
+println("Quantidade index lista vermelha $quantidadeIndex")
