0d8f357eaa3e896261137b448886ef4b647c4d25
Rodrigo Nogueira <digo22nt@gmail.com>
2026-08-09 14:48:20 -0300

    Exercicio 02 Declarar tipos corretos concluido.

---
+++ ex02-DeclararTiposCorretos/DeclararTiposCorretos.kts
@@ -0,0 +1,8 @@
+    val a: Int = 1000;
+    val b: String = "Log message";
+    val c: Double = 3.14;
+    val d: Long = 100_000_000L;
+    val e: Boolean = false;
+    val f: Char = '\n';
+
+    println(" TESTE $a");
