569583059303be504749b32930d8ef4823125cb8
Rodrigo Nogueira <digo22nt@gmail.com>
2026-08-10 13:51:19 -0300

    ex15, exs segurança nula concluidos

---
+++ ex15-segurancaNula/PlataformaMidia.kt
@@ -0,0 +1,18 @@
+data class User(val username: String, val isActive: Boolean)
+
+fun getActiveUsernames(users: List<User>): List<String> {
+    return users.mapNotNull { user ->
+        if (user.isActive) user.username else null
+    }
+}
+
+fun main() {
+    val allUsers = listOf(
+        User("alice123", true),
+        User("bob_the_builder", false),
+        User("charlie99", true)
+    )
+
+    println(getActiveUsernames(allUsers))
+    // [alice123, charlie99]
+}
