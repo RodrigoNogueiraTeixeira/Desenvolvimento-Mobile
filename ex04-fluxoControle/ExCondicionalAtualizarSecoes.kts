0340a41e84adc43f576ec6b237378e68ad3e58bc
Rodrigo Nogueira <digo22nt@gmail.com>
2026-08-10 12:01:46 -0300

    ex04, ex condicional atualizar secoes concluido

---
+++ ex04-fluxoControle/ExCondicionalAtualizarSecoes.kts
@@ -0,0 +1,12 @@
+    val button = "A"
+
+    println(
+        when (button) {
+            "A" -> "Yes"
+            "B" -> "No"
+            "X" -> "Menu"
+            "Y" -> "Nothing"
+            else -> "There is no such button"
+        }
+    )
+
